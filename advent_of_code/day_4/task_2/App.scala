a5ef0068410850aeb31816118c01805231993f37
Pavel Popov <Pavel.Popov@dataart.com>
2021-12-05 14:36:13 +0300

    Add task description for Day 4

--- advent_of_code/day_4/task_2/App.scala
+++ advent_of_code/day_4/task_2/App.scala
@@ -1,10 +1,26 @@
-
+/*
+ * On the other hand, it might be wise to try a different strategy:
+ * let the giant squid win.
+ *
+ * You aren't sure how many bingo boards a giant squid could play at once,
+ * so rather than waste time counting its arms, the safe thing to do is to
+ * figure out which board will win last and choose that one. That way, no
+ * matter which boards it picks, it will win for sure.
+ *
+ * In the above example, the second board is the last to win, which happens
+ * after 13 is eventually called and its middle column is completely marked.
+ * If you were to keep playing until this point, the second board would have a
+ * sum of unmarked numbers equal to 148 for a final score of 148 * 13 = 1924.
+ *
+ * Figure out which board will win last. Once it wins, what would its
+ * final score be?
+ *
+ */
 import scala.io.Source
 import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 import Integer.parseInt
 import javax.swing.plaf.basic.BasicBorders.MarginBorder
-
 
 val boardSize = 5
 
@@ -97,3 +113,4 @@ def calcWinScore(scores: List[Int], boards: List[Board]): Option[Int] = {
 def main(args: Array[String]): Unit = println {
   calcWinScore.tupled(readInput(taskInput))
 }
+
